64a0c1b40b0bcf7d0648926cadc4054600fd003e
Sean Monroe <seanmonroe86@gmail.com>
2017-04-22 17:04:48 -0400

    repo init

---
+++ run.py
@@ -0,0 +1,60 @@
+import numpy as np
+import flight_cam as flight
+import cv2, fcntl, multiprocessing, os, select, socket, struct, sys
+import tempfile, threading, time, thread, signal, subprocess, termios
+
+
+def drone_act(drone, in_list, com):
+    if com == "z": in_list.remove(file)
+    return in_list
+
+
+def print_bat(drone):
+    bat = drone.getBattery()
+    drone.printBlue("Battery: {}% {}".format(bat[0], bat[1]))
+
+
+def drone_init(drone):
+    drone.startup()
+    drone.reset()
+    while drone.getBattery()[0] == -1: time.sleep(0.1)
+    time.sleep(0.5)
+    print_bat(drone)
+    drone.useDemoMode(False)
+    drone.setConfigAllID()
+    drone.hdVideo(False)
+    drone.frontCam(False)
+    CDC = drone.ConfigDataCount
+    while CDC == drone.ConfigDataCount: time.sleep(0.0001)
+    drone.startVideo()
+
+
+# init
+drone = flight.Drone()
+drone_init(drone)
+
+# main loop
+stop, IMC, read_list, bat_time = False, drone.VideoImageCount, [sys.stdin], time.time()
+while read_list:
+    while drone.VideoImageCount == IMC: time.sleep(0.1)
+    IMC = drone.VideoImageCount
+
+    # video image
+    img = drone.VideoImage
+    img_out = cv2.resize(img, (120, 160))
+
+    # show video
+    cv2.imshow("video", img_out)
+    cv2.waitKey(1)
+
+    # get keypress and act
+    ready = select.select(read_list, [], [], 0.1)[0]
+    if ready:
+        for file in ready:
+            read_list = drone_act(drone, read_list, file.readline()[0])
+    #if time.time() - bat_time >= 15: print_bat(drone)
+
+
+# drone has shut down, print battery and status
+print_bat(drone)
+
