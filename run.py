6d4a81a02d249f1f0c4750b5fcc98afdd2a4a0b0
Sean Monroe <seanmonroe86@gmail.com>
2017-04-24 10:07:09 -0400

    moved image capture to new file

--- run.py
+++ run.py
@@ -3,69 +3,69 @@ import flight_cam as flight
 import cv2, fcntl, multiprocessing, os, select, socket, struct, sys
 import tempfile, threading, time, thread, signal, subprocess, termios
 
-
-def getGPS(drone):
-    big = drone.ConfigData
-    lat, lon, alt = float(big[129][1]), float(big[130][1]), float(big[131][1])
-    return lat, lon, alt
-
+def simple_flight(drone):
+    drone.takeoff()
+    time.sleep(5)
+    drone.setSpeed(0.2)
+    time.sleep(1)
+    drone.moveForward()
+    time.sleep(2)
+    drone.stop()
+    time.sleep(1)
+    drone.moveBackward()
+    time.sleep(2)
+    drone.stop()
+    drone.land()
 
 def drone_act(drone, in_list, com):
-    if com == "z": in_list.remove(file)
+    if com == "z":
+        drone.land()
+        in_list.remove(file)
+    elif com == "s":
+        simple_flight(drone)
     return in_list
 
 
 def print_bat(drone):
     bat = drone.getBattery()
-    drone.printBlue("Battery: {}% {}".format(bat[0], bat[1]))
+    drone.printBlue("Battery: {}% {}\n\n".format(bat[0], bat[1]))
 
 
 def drone_init(drone):
     drone.startup()
     drone.reset()
-    while drone.getBattery()[0] == -1: time.sleep(0.1)
-    time.sleep(0.5)
+    while drone.getBattery()[0] == -1: time.sleep(0.01)
+    time.sleep(0.1)
     print_bat(drone)
     drone.useDemoMode(False)
     drone.setConfigAllID()
-    drone.hdVideo(False)
-    drone.frontCam(False)
+    drone.groundCam()
+    drone.midVideo()
+    drone.sdVideo()
     CDC = drone.ConfigDataCount
     while CDC == drone.ConfigDataCount: time.sleep(0.0001)
-    drone.startVideo()
 
 
 # init
 drone = flight.Drone()
 drone_init(drone)
-init_lat, init_lon, init_alt = getGPS(drone)
 
 # main loop
-stop, IMC, read_list, bat_time = False, drone.VideoImageCount, [sys.stdin], time.time()
-while read_list:
-    while drone.VideoImageCount == IMC: time.sleep(0.1)
-    IMC = drone.VideoImageCount
-    lat, lon, alt = getGPS(drone)
+stop, read_list, timer = False, [sys.stdin], time.time()
+try:
+    while read_list:
+        # get keypress and act
+        ready = select.select(read_list, [], [], 0.1)[0]
+        if ready:
+            for file in ready:
+                read_list = drone_act(drone, read_list, file.readline()[0])
+        if (time.time() - timer) >= 15:
+            print_bat(drone)
+            timer = time.time()
 
-    # video image
-    img = drone.VideoImage # 640x360
-    img = img[:, 185:455]
-    img_out = cv2.resize(img, (120, 160))
-
-    # show video
-    cv2.imshow("video", img_out)
-    cv2.waitKey(1)
-
-    # get keypress and act
-    ready = select.select(read_list, [], [], 0.1)[0]
-    if ready:
-        for file in ready:
-            read_list = drone_act(drone, read_list, file.readline()[0])
-    if (time.time() - bat_time) >= 15:
-        print_bat(drone)
-        bat_time = time.time()
-
-
+except KeyboardInterrupt as e:
+    print e
+    
 # drone has shut down, print battery and status
 print_bat(drone)
 
