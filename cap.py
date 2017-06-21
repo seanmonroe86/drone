2ea1361e5d5f0c4033dedf5b600a58a5a0509bea
Sean Monroe <seanmonroe86@gmail.com>
2017-06-21 13:03:11 -0400

    added test to check camera

--- cap.py
+++ cap.py
@@ -1,6 +1,25 @@
 import cv2, time
 import numpy as np
 from threading import Thread
+from socket import socket, AF_INET, SOCK_STREAM
+
+class VidSocket:
+    def __init__(self):
+        self.IP = "192.168.1.1"
+        self.PORT = 5555
+        self.socket = socket(AF_INET, SOCK_STREAM)
+        self.socket.connect((self.IP, self.PORT))
+
+    def get(self):
+        chunks = []
+        bytes_recd = 0
+        while bytes_recd < 2048:
+            chunk = self.socket.recv(2048)
+            if chunk == '':
+                raise RuntimeError("socket connection broken")
+            chunks.append(chunk)
+            bytes_recd += len(chunk)
+        return ''.join(chunks)
 
 class ImageGetter:
     def __init__(self):
@@ -13,12 +32,15 @@ class ImageGetter:
         self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT,self.CAMERA_HEIGHT)
 
     def start(self):
-        Thread(target=self.updateFrame, args=()).start()
+        cap = Thread(target=self.updateFrame, args=())
+        cap.daemon = True
+        cap.start()
 
     def updateFrame(self):
         while(True):
-            ret, self.currentFrame = self.capture.read()
+            ret, frame = self.capture.read()
             while not ret: ret, frame = self.capture.read()
+            self.currentFrame = frame
 
     def getFrame(self):
         return self.currentFrame
@@ -26,21 +48,20 @@ class ImageGetter:
     def release(self):
         return self.capture.release()
 
-rec = cv2.VideoWriter('out.divx', cv2.VideoWriter_fourcc(*'DIVX'), 20.0, (640, 360))
-cap = ImageGetter()
-cap.start()
-time.sleep(1)
-try:
-    while True:
-        img = cap.getFrame()
-        try:
-            test_img = img[:,:]
-        except TypeError:
-            continue
-        rec.write(img)
-        cv2.imshow("video", img)
-        cv2.waitKey(1)
-except KeyboardInterrupt:
-    rec.release()
-    pass
+#cap = ImageGetter()
+#cap.start()
+cap = VidSocket()
+print cap.get()
+#time.sleep(1)
+#try:
+#    while True:
+#        img = cap.getFrame()
+#        #try:
+#        #    test_img = img[:,:]
+#        #except TypeError:
+#        #    continue
+#        cv2.imshow("video", img)
+#        cv2.waitKey(1)
+#except KeyboardInterrupt:
+#    cap.release()
 
